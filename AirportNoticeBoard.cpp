3dbec7b7eac4c72a288bf91e78b2dc5e1471ec3f
Kevintarada <kevintarada_12@yahoo.com>
2018-05-26 20:10:07 +0700

    random chance for delay added

--- AirportNoticeBoard.cpp
+++ AirportNoticeBoard.cpp
@@ -31,7 +31,6 @@ struct node{
 	string remarks;
 	string country;
 	string time;
-	
 };
 
 node arr[15];
@@ -39,71 +38,69 @@ queue<int> dep;
 queue<int> land;
 queue<int> empty;
 
-
 string Remarks[] = {
-	"Landing","Refuel","On Schedule","Boarding","Final Check","Departed","Delayed"
+	"Landing","Refuel","On Schedule","Delayed","Boarding","Final Check","Departed"
 };
-
 
 string remarks(int index) { //Generate Remarks (Sequencial)
 
 	string remarks;
 
 	remarks = Remarks[index];
-	
+
 	return remarks;
 }
 
 string flight() { //Generate Random Flight Number
-	
+
 	char letter;
 	int random;
 	string number;
-	
+
 	string flightcode;
-	
+
 	for (int i=0; i<2; i++) {
 		random = rand() % 26;
 		letter = 'A' + random;
 		flightcode += letter;
 	}
-	
+
 	flightcode += " ";
-	
+
 	for (int i=0; i<4; i++) {
 		random = rand() % 10;
 		number = to_string(random);
 		flightcode += number;
 	}
-		
+
 	return flightcode;
 }
 
 string country() { //Generate Random Country
-	
+
 	string arr[] = {
 	"London","Alaska","Boston","Madrid","Queens",
 	"Athens","Berlin","Moscow","Beirut","Bogota",
 	"Havana","Dublin","Manila","Monaco","Prague",
 	"Vienna","Warsaw","Taipei","Lisbon"
 	};
-	
+
 	string country;
 	int random;
-	
+
 	random = rand() % (sizeof(arr)/sizeof(*arr));
 	country = arr[random];
 	return country;
 }
 
 string time() { //Generate Random Time
-	
 
-string clock;
-	
+
+	string clock;
+
 	time_t theTime = time(NULL);
 	struct tm *aTime = localtime(&theTime);
-	
+
 	int hour = aTime->tm_hour;
 	int min = aTime->tm_sec;
 	clock += to_string(hour);
@@ -114,12 +111,12 @@ string clock;
 	}else {
 		clock += to_string(min);
 	}
-	
+
 	return clock;
 }
 
 void announcement(queue<int> dep, queue<int> lan, node arr[]){
-	
+
 	int x;
 	while(!dep.empty()){
 		x = dep.front();
@@ -131,7 +128,7 @@ void announcement(queue<int> dep, queue<int> lan, node arr[]){
 		lan.pop();
 		cout<<arr[x].time<<setw(9)<<" flight No."<<arr[x].flight<<" has landed"<<endl;
 	}
-	
+
 }
 
 void newData(queue<int> dep){
@@ -139,7 +136,7 @@ void newData(queue<int> dep){
 	while(!dep.empty()){
 		y=dep.front();
 		dep.pop();
-		
+
 		node obj;
 		obj.country=country();
 		obj.flight=flight();
@@ -150,41 +147,41 @@ void newData(queue<int> dep){
 		arr[y].flight=obj.flight;
 		arr[y].remarks=obj.remarks;
 		arr[y].time=obj.time;
-		
+
 	}
 }
 
 void checkData(){
-	
+
 	for (int i=0; i</*(sizeof(arr)/sizeof(*arr))*/15; i++){
 		if(arr[i].remarks == "Departed"){
 			dep.push(i);
-			
+
 		}else if(arr[i].remarks=="Landing"){
 			land.push(i);
-		}		
+		}
 	}
 }
 
 void declaredata(){
-	
+
 	for (int i=0; i<(sizeof(arr)/sizeof(*arr)); i++){
-		
+
 		node obj;
-		
+
 		obj.flight=flight();
 		obj.time=time();
 		obj.country=country();
 		obj.remarks=remarks(0);
-		
+
 		arr[i] = obj;
 	}
 }
 
 void printdata(){
-	
+
 	for (int i=0;i<(sizeof(arr)/sizeof(*arr));i++){
-			cout 
+			cout
 			<<"||"<< setw(9)<< arr[i].flight << setw(9)
 			<<"||"<< setw(9) << arr[i].country << setw(9)
 			<<"||"<< setw(9) << arr[i].time << setw(9)
@@ -195,70 +192,74 @@ void printdata(){
 
 void update() {
 	int random;
+	int random2;
 	int x;
 	int r;
-	
+
 	r = rand() % 6 + 1;
-	
+
 	for (int i=0; i<r; i++) { // Calculate how many Remarks are updated (randomly)
 		random = rand() % (sizeof(arr)/sizeof(*arr));
-	
+
 		if (arr[random].remarks != "") {
 			if (arr[random].remarks == Remarks[0] ) x = 1;
 			else if (arr[random].remarks == Remarks[1] ) x = 2;
-			else if (arr[random].remarks == Remarks[2] ) x = 3;
+			else if (arr[random].remarks == Remarks[2] ){
+				random2 = rand();
+				if (random2%2 == 0){
+					x = 3;
+				}else {
+					x = 4;
+				}
+			}
 			else if (arr[random].remarks == Remarks[3] ) x = 4;
 			else if (arr[random].remarks == Remarks[4] ) x = 5;
 			else if (arr[random].remarks == Remarks[5] ) x = 6;
 			arr[random].remarks = remarks(x);
 		}
 	}
-	
+
 	for (int k = 0; k<empty.size();k++){
 		int x = empty.front();
-		
+
 		arr[x].country=country();
 		arr[x].flight=flight();
 		arr[x].time=time();
 		arr[x].remarks=remarks(0);
-		
+
 		empty.pop();
 	}
-	
+
 	for (int j = 0; j<dep.size();j++){
 		int x = dep.front();
-		
+
 		arr[x].remarks = "";
 		arr[x].flight = "";
 		arr[x].time = "";
 		arr[x].country = "";
-		
+
 		dep.pop();
 		empty.push(x);
 	}
-	
-	clearQueue(dep, land);
-} 
 
+	clearQueue(dep, land);
+}
 
 int main(){
 	srand(time(NULL));
-	
+
 	declaredata();
-	
+
 	while(true){
 		printdata(); //cout << //"print ";
 		checkData(); //cout << //"check ";
 		sleep(1);
 		update(); //cout << "update "<< endl;
-		
+
 		gotoxy(0,0);
 	//	system("cls");
-
-		
 	}
-		
+
 	cin.ignore();
 	return 0;
 }
-
